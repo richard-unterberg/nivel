15353b488da7c31bc5568c1343d317407e4d7874
Richard Unterberg <richard.unterberg@lipsia.digital>
2026-03-30 20:46:17 +0200

    improve alert

--- components/docs/Alert.tsx
+++ components/docs/Alert.tsx
@@ -1,19 +1,50 @@
-import cm from '@classmatejs/react'
+import cm, { cmMerge } from '@classmatejs/react'
+import { Check, CircleX, Info, TriangleAlert } from 'lucide-react'
+import type { ReactNode } from 'react'
 
 type AlertVariant = 'info' | 'warning' | 'error' | 'success'
+
+const alertIconMap = {
+  info: Info,
+  warning: TriangleAlert,
+  error: CircleX,
+  success: Check,
+}
 
 export const Alert = ({
   type = 'info',
   heading,
   children,
+  icon = true,
 }: {
   type?: AlertVariant
-  heading?: React.ReactNode
-  children: React.ReactNode
+  heading?: ReactNode
+  children: ReactNode
+  icon?: boolean | ReactNode
 }) => {
+  const AlertIcon = icon === true ? alertIconMap[type] : typeof icon === 'object' ? () => <>{icon}</> : null
+
+  const alertIconTextColorClass = {
+    info: 'text-info',
+    warning: 'text-warning',
+    error: 'text-error',
+    success: 'text-success',
+  }[type]
+
   return (
     <AlertOuter $variant={type}>
-      {heading && <AlertHeading>{heading}</AlertHeading>}
+      {!!heading && AlertIcon && (
+        <div className="flex items-center gap-2 mb-3">
+          <AlertIcon className={cmMerge(alertIconTextColorClass, 'float-left w-5 h-5 ')} />
+          <AlertHeading>{heading}</AlertHeading>
+        </div>
+      )}
+      {!(!!heading && AlertIcon) && !!heading && <AlertHeading>{heading}</AlertHeading>}
+      {!(!!heading && AlertIcon) && AlertIcon && (
+        <div className={cmMerge(alertIconTextColorClass, 'float-left mr-2')}>
+          <AlertIcon className="w-5 h-5 mt-1" />
+        </div>
+      )}
       {children}
     </AlertOuter>
   )
@@ -25,9 +56,8 @@ const AlertOuter = cm.section.variants<{ $variant: AlertVariant }>({
     mb-5
     border
     rounded-lg
-    prose-p:leading-7
     prose-p:last:mb-0
-    prose-p:first:mt-0
+    prose-p:mt-0
     prose-headings:first:mt-0
     prose-headings:last:mb-0
     prose-ul:first:mt-0
@@ -37,10 +67,10 @@ const AlertOuter = cm.section.variants<{ $variant: AlertVariant }>({
   `,
   variants: {
     $variant: {
-      info: 'bg-info/5 border-info/30',
-      warning: 'bg-warning/5 border-warning/30',
-      error: 'bg-error/5 border-error/25',
-      success: 'bg-success/5 border-success/35',
+      info: 'bg-info/10 border-info/30',
+      warning: 'bg-warning/10 border-warning/30',
+      error: 'bg-error/10 border-error/25',
+      success: 'bg-success/10 border-success/35',
     },
   },
   defaultVariants: {
@@ -49,8 +79,6 @@ const AlertOuter = cm.section.variants<{ $variant: AlertVariant }>({
 })
 
 const AlertHeading = cm.header`
-  mt-1
   font-bold
   text-base
-  mb-5
 `
